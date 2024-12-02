952b2d37efc106fc9798eaea6572a5b8801acb9f
medaymenjandoubi <medaymenjandoubi4@gmail.com>
2024-12-02 11:59:57 +0100

    commit 02_12_2024_11:59

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Test Route
+app.get('/', (req, res) => {
+    res.send('Majota Backend is running!');
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
